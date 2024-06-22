edbbd43a9573eef91ceecd81843ed445ac36e0c2
dion <dion@jsm.co.id>
2024-06-22 15:09:41 +0700

    schedule task: resend failed message to webhook client, update satus message template. rename helper webhhok client api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
+import cron from "node-cron";
 import config from "./config";
 import router from "./routes";
 import { readContent, writeContent } from "./helpers/file";
 import { randomString } from "./helpers/value";
 import * as logger from "./helpers/logger";
+import * as scheduleTask from "./helpers/schedule-task";
 
 const app: Express = express();
 const { env, port } = config;
@@ -36,4 +38,43 @@ app.listen(port, async (err?: Error) => {
     }
 
     console.log(`[server] is running for ${env} environtment | port ${port}`);
+});
+
+// running schedule task every 2 minutes
+let isResendFailedMesssageInbound: boolean = false;
+cron.schedule('*/2 * * * *', async () => {
+    if (isResendFailedMesssageInbound) {
+        console.log(`[schedule-task] resend failed message inbound to webhook client api is still running...`);
+        return false;
+    }
+
+    isResendFailedMesssageInbound = true;
+    await scheduleTask.resendFailedMesssageInbound();
+    isResendFailedMesssageInbound = false;
+});
+
+// running schedule task every 5 minutes
+let isResendFailedMesssageNotif: boolean = false;
+cron.schedule('*/5 * * * *', async () => {
+    if (isResendFailedMesssageNotif) {
+        console.log(`[schedule-task] resend failed message notification to webhook client api is still running...`);
+        return false;
+    }
+
+    isResendFailedMesssageNotif = true;
+    await scheduleTask.resendFailedMesssageNotif();
+    isResendFailedMesssageNotif = false;
+});
+
+// running schedule task every 15 minutes
+let isUpdateTemplateStatus: boolean = false;
+cron.schedule('*/15 * * * *', async () => {
+    if (isUpdateTemplateStatus) {
+        console.log(`[schedule-task] update template status from whatsapp api is still running...`);
+        return false;
+    }
+
+    isUpdateTemplateStatus = true;
+    await scheduleTask.updateTemplateStatus();
+    isUpdateTemplateStatus = false;
 });
